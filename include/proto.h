0ecc879036d9737672bd0149815bd95ed6091970
MysterJacob <40712909+MysterJacob@users.noreply.github.com>
2024-11-13 20:35:05 +0100

    datatypes, first try of parserTable

--- include/proto.h
+++ include/proto.h
@@ -3,6 +3,20 @@
 #define MAGIC1 0x57
 #define MAGIC2 0x5f
 
+enum type {
+  TYPE_INT8    = 0,
+  TYPE_INT16   = 1,
+  TYPE_INT32   = 2,
+  TYPE_INT64   = 3,
+  TYPE_VARINT  = 4,
+  TYPE_UINT8   = 5,
+  TYPE_UINT16  = 6,
+  TYPE_UINT32  = 7,
+  TYPE_UINT64  = 8,
+  TYPE_VARUINT = 9,
+  TYPE_STRING  = 10
+};
+
 typedef volatile struct {
   unsigned int length;
   unsigned int id;
@@ -11,8 +25,14 @@ typedef volatile struct {
   unsigned int checksum;
 } PacketHeader;
 
+#ifndef PACKET_COUNT
+#define PACKET_COUNT 0
+#endif
+
+extern unsigned char* parserTable[PACKET_COUNT];
+
 void processByte(byte data);
-PacketHeader *getLastHeader();
+PacketHeader* getLastHeader();
 void resetParsing();
 
 void* getLastPacket();
